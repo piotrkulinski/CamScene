77a01fb15420716bb4e7c50106d0fb49ef49fe74
Piotr Kuliński <piotr.kulinski@gmail.com>
2025-04-17 13:52:56 +0200

    uruchomienie z ustawieniem środowiska

---
+++ ca.bat
@@ -0,0 +1,3 @@
+set path=%path%;c:\msys64\mingw64\bin;p:\vcpkg\packages\openexr_x64-windows\bin;c:\msys64\home\piotr\opencv_install\x64\mingw\bin
+"C:\Qt\6.9.0\mingw_64\bin\windeployqt.exe" camscene.exe
+call camscene.exe
